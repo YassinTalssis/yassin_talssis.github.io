85300528f1757baa39db934520584dcb0de026ce
Yassin TALSSIS <47031764+YassinTalssis@users.noreply.github.com>
2024-08-22 15:27:23 +0100

    update

--- assets/js/project.js
+++ assets/js/project.js
@@ -11,7 +11,7 @@ const projects = [
     description: "Building a basic search engine with python and Elasticsearch.",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
     Previewlink: "",
-    Githublink: "",
+    Githublink: "https://github.com/YassinTalssis/Search_engine-V1.0",
   },
   {
     title: "Web Scraper",
@@ -19,7 +19,7 @@ const projects = [
     description: "A program that downloads all the data of a website, extracts all links and adds them to waiting and visited lists.",
     tagimg: "https://image.flaticon.com/icons/png/512/643/643350.png",
     Previewlink: "",
-    Githublink: "",
+    Githublink: "https://github.com/YassinTalssis/Web_Scraper",
   },
   {
     title: "Instagram Fake and Real Accounts classification",
@@ -27,7 +27,7 @@ const projects = [
     description: "Accounts classification using Machine Learning",
     tagimg: "https://cdn.iconscout.com/icon/free/png-512/react-1-282599.png",
     Previewlink: "",
-    Githublink: "",
+    Githublink: "https://github.com/YassinTalssis/Instagram-Fake-Accounts-Detection",
   },
   {
     title: "sEMG‑Based Hand’s ﬁngers gestures Recognition",
@@ -43,7 +43,7 @@ const projects = [
     description: "This project aimed to create a dashboard using Streamlit to automate client reporting.",
     tagimg:"",
     Previewlink: "",
-    Githublink: "",
+    Githublink: "https://github.com/YassinTalssis/Dynamic-Dashboard",
   },
   {
     title: "Human Action Recognition",
@@ -61,7 +61,7 @@ const projects = [
     tagimg:
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
     Previewlink: "",
-    Githublink: "",
+    Githublink: "https://github.com/YassinTalssis/Tomatoes-maturity-Classification/tree/master",
   },
   {
     title: "Gastrointestinal Diseases Classification Using Transfer Learning",
@@ -70,7 +70,7 @@ const projects = [
     tagimg:
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
     Previewlink: "",
-    Githublink: "",
+    Githublink: "https://github.com/YassinTalssis/Classification-of-brain-cancer-with-ML-and-dataset-Figshare",
   },
 ];
 
